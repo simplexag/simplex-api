7ca264df792f65cffbbfc9e632dd0cf542f8ef1d
Aaron Hunt <aaronhunt@Aarons-MacBook-Pro.local>
2024-01-17 21:24:44 -0500

    print saved rx

--- samples/models.py
+++ samples/models.py
@@ -68,6 +68,20 @@ class SoilExtraction(SoftDeleteModel):
 
 ###### End Soil Extraction Reference Tables
 
+#### System Labs ####################
+"""
+class Labs(SoftDeleteModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    display_name = models.CharField(max_length=20,blank=False,null=False)
+    full_name = models.CharField(max_length=20,blank=False,null=False)
+
+class AccountLabs(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, blank=False, null=False)
+    lab = models.ForeignKey(Labs, on_delete=models.CASCADE, blank=False, null=False)
+"""
+#### End Labs #######################
+
 #Holds all sample event types
 class SampleEvent(models.Model):
     #Enum for the different 
